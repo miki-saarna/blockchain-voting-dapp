6ff9e00ff286da62d697876ac5df6c3093c64980
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-02-16 13:02:29 -0800

    Init Vault interface

---
+++ src/interfaces/IVault.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {IVoteToken} from "./IVoteToken.sol";
+
+interface IVault {
+  function vaultInitialized() external returns (bool);
+
+  function initVault(IVoteToken voteToken, address managerContract) public;
+}
