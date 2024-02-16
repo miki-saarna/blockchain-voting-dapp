d8dbfa89090d09f24eeade9aeb5a79b7c1290d77
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-02-16 13:58:30 -0800

    Remove unused code

--- src/interfaces/IVote.sol
+++ src/interfaces/IVote.sol
@@ -31,38 +31,3 @@ interface IVote {
 
   function checkIfSenderAlreadyVoted() external view returns (bool);
 }
-
-// interface ISoulmate {
-//     function idToCreationTimestamp(uint256 id) external returns (uint256);
-
-//     function soulmateOf(address soulmate) external returns (address);
-
-//     function sharedSpace(uint256 id) external returns (string memory);
-
-//     function ownerToId(address owner) external returns (uint256);
-
-//     function mintSoulmateToken() external returns (uint256);
-
-//     function tokenURI(uint256) external pure returns (string memory);
-
-//     function transferFrom(address from, address to, uint256 id) external;
-
-//     function writeMessageInSharedSpace(string calldata message) external;
-
-//     function readMessageInSharedSpace()
-//         external
-//         view
-//         returns (string memory message);
-
-//     function getDivorced() external;
-
-//     function isDivorced() external view returns (bool);
-
-//     function totalSupply() external view returns (uint256);
-
-//     function totalSouls() external view returns (uint256);
-
-//     function checkIfSenderFound() external view;
-    
-//     function checkIfSoulmateFound() external view;
-// }
