f42409751d31620b3f8420de836ff26b1ca75573
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-02-15 22:42:06 -0800

    Created pull mechanism for bonus reward

--- src/Vote.sol
+++ src/Vote.sol
@@ -5,19 +5,25 @@ pragma solidity ^0.8.23;
 
 contract Vote {
 
+  error Vote__PollNotClosed();
   error Vote__PollClosed(uint256 pollEndTime);
   error Vote__UserAlreadyVoted(address owner, string candidate);
   error Vote__AllRegisteredVotersAlreadyVoted(address owner);
+  error Vote__UserHasNotVoted;
+  error Vote__UserAlreadyClaimedRewardBonus;
   
   uint256 voteId = 1;
   uint256 pollEndTime;
   uint256 voteRewardAmount = 50;
+  uint256 voteRewardBonusAmount;
   uint256 voteRewardBonusMaxAmount = 1000;
   uint256 totalVoteCount = 0;
   uint256 totalRegisteredVoters = 3000000;
 
   mapping(uint256 id => address owner) idToOwners;
   mapping(address owner => uint256 id) ownerToId;
+
+  mapping(address owner => bool claimed) claimedRewardBonus;
 
   mapping(address owner => uint256 candidateIdx) ownerToCandidateIdx;
 
@@ -92,8 +98,7 @@ contract Vote {
       winner = winners[0];
     }
 
-    uint256 rewardBonusAmount = getTokenRewardBonusAmount()
-    // voteToken.transfer(address(voteVault), msg.sender, rewardBonusAmount); // need to alter this to send bonus reward to ALL who voted
+    uint256 voteRewardBonusAmount = getTokenRewardBonusAmount()
 
     return winner;
   }
@@ -104,6 +109,20 @@ contract Vote {
       return RewardBonusAmount;
     } else {
       return 1;
+    }
+  }
+
+  // Pull Mechanism:
+  function claimRewardBonus() {
+    if (pollEndTime == 0) {
+      revert Vote__PollNotClosed();
+    } else if (!ownerToCandidateIdx[msg.sender]) {
+      revert Vote__UserHasNotVoted();
+    } else if (claimedRewardBonus[msg.sender]) {
+      revert Vote__UserAlreadyClaimedRewardBonus();
+    } else {
+      claimedRewardBonus[msg.sender] = true
+      voteToken.transferFrom(address(voteVault), msg.sender, voteRewardBonusAmount)
     }
   }
 }
