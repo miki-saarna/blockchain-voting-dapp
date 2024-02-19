75ff9db589e23c37382a6e8b88dce154829157df
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-02-19 14:13:49 -0800

    Simplified deploy contract

--- script/DeployVote.s.sol
+++ script/DeployVote.s.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
+import "forge-std/console.sol";
+
 import {Script} from "forge-std/Script.sol";
 import {Vote} from "../src/Vote.sol";
 import {VoteToken} from "../src/VoteToken.sol";
@@ -11,19 +13,17 @@ import {IVoteToken} from "../src/interfaces/IVoteToken.sol";
 import {IVault} from "../src/interfaces/IVault.sol";
 
 contract DeployVote is Script {
-  function run() public returns (Vote) {
+  event VoteContractDeployed(address indexed voteAddress);
+
+  function run() public {
     vm.startBroadcast();
 
     Vault vault = new Vault();
     VoteToken voteToken = new VoteToken(address(vault));
-
-    // IVoteToken iVoteToken = IVoteToken(address(voteToken));
     vault.initVault(voteToken, address(this));
-
     Vote vote = new Vote(voteToken, vault);
+    // emit VoteContractDeployed(address(vote));
 
     vm.stopBroadcast();
-
-    return vote;
   }
 }
