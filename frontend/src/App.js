581927ecac987c8db274ffab1b17283776e1227f
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 14:46:17 -0700

    Restructured main content into separate components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@
 import React, { useState, useEffect } from 'react';
 import Header from './header/index';
 import Footer from './footer/index';
-import PollDetails from './details/pollDetails';
+import Intro from './intro/index';
+import Voting from './voting/index'
 import { getProvider, getSigner, getContract} from './utils/blockchainInteractions';
 
 function App() {
   const [userAccount, setUserAccount] = useState(null);
   const [candidates, setCandidates] = useState([]);
-  const [pollStartTime, setPollStartTime] = useState(null);
-  const [pollEndTime, setPollEndTime] = useState(null);
   const [voteCount, setVoteCount] = useState({});
   const [winner, setWinner] = useState([]);
 
@@ -29,20 +28,10 @@ function App() {
 
   useEffect(() => {
     connectWallet();
-    fetchPollDetails();
-    fetchCandidates();
+    // fetchCandidates();
     // Optional: Listen for account changes
     window.ethereum.on('accountsChanged', () => connectWallet());
   }, []);
-
-  const fetchPollDetails = async () => {
-    const provider = getProvider();
-    const contract = getContract(provider);
-    const startTime = await contract.pollStartTime();
-    const endTime = await contract.pollEndTime();
-    setPollStartTime(startTime.toString());
-    setPollEndTime(endTime.toString());
-  };
 
   const fetchCandidates = async () => {
     const provider = getProvider();
@@ -57,21 +46,6 @@ function App() {
     setCandidates(candidatesList);
     console.log(candidates)
   };
-
-  async function beginPoll() {
-    const signer = getSigner()
-    const contract = getContract(signer)
-
-    const exampleCandidates = ["jack", 'henz', 'lemon']
-
-    const tx = await contract.beginPoll(exampleCandidates)
-    console.log("tx", tx)
-    await tx.wait()
-    console.log('tx mined')
-    const startTime = await contract.pollStartTime();
-    setPollStartTime(startTime.toString());
-    setPollEndTime(0);
-  }
 
   const endPoll = async () => {
     const signer = await getSigner();
@@ -88,31 +62,18 @@ function App() {
     return candidates;
   }
 
-  async function castVote(candidateIdx) {
-    const signer = getSigner()
-    const contract = getContract(signer)
-    const tx = await contract.castVote(candidateIdx)
-    console.log("tx", tx)
-    await tx.wait()
-    console.log('tx mined')
-  }
+  
 
   return (
     <div className="App flex flex-col min-h-screen">
       <Header />
-      <main className="grow">
-        <PollDetails
-          pollStartTime={pollStartTime}
-          pollEndTime={pollEndTime}
-          candidates={candidates}
-        />
-
-        <p>Your account: {userAccount}</p>
-        <button className="border border-gray-400 px-2 py-1 rounded" onClick={connectWallet}>Connect to MetaMask</button>
-        <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={beginPoll}>Begin poll</button>
-        <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={fetchCandidates}>Get list of candidates</button>
-        <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={() => castVote(1)}>Vote for 2nd candidate</button>
-      </main>
+      <Intro />
+      <button className="border border-gray-400 px-2 py-1 rounded" onClick={connectWallet}>Connect to MetaMask</button>
+      <p>Your account: {userAccount}</p>
+      <Voting
+        candidates={candidates}
+      />
+      <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={fetchCandidates}>Get list of candidates</button>
       <Footer />
     </div>
   );
---
+++ frontend/src/voting/index.tsx
@@ -0,0 +1,51 @@
+import { JSX, useState, useEffect } from 'react';
+import Details from './details';
+import Poll from './poll';
+import Rewards from './rewards';
+import { getProvider, getSigner, getContract} from '../utils/blockchainInteractions';
+
+export default function Voting({
+  candidates
+}: {
+  candidates: any
+}): JSX.Element {
+
+  const [pollStartTime, setPollStartTime] = useState<Date | null>(null);
+  const [pollEndTime, setPollEndTime] = useState<Date | null>(null);
+
+  const fetchPollDetails = async () => {
+    const provider = getProvider();
+    const contract = getContract(provider);
+    const startTime = await contract.pollStartTime();
+    const endTime = await contract.pollEndTime();
+    setPollStartTime(convertBigIntToDate(startTime));
+    setPollEndTime(convertBigIntToDate(endTime));
+  };
+
+  function convertBigIntToDate(bigInt: BigInt): Date | null {
+    const timestamp = Number(bigInt);
+    if (!timestamp) return null
+    const date = new Date(timestamp * 1000);
+    return date
+  }
+
+  useEffect(() => {
+    fetchPollDetails();
+    // fetchCandidates();
+    // Optional: Listen for account changes
+    // window.ethereum.on('accountsChanged', () => connectWallet());
+  }, []);
+
+
+  return (
+    <main className="grow max-w-2xl lg:max-w-6xl flex flex-col space-y-6 p-6 lg:px-12">
+      <Details
+        pollStartTime={pollStartTime}
+        pollEndTime={pollEndTime}
+        candidates={candidates}
+      />
+      <Poll />
+      <Rewards />
+    </main>
+  )
+}
