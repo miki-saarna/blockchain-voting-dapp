2e3218b024b49bfe664d660206bd751d50c07513
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 14:45:57 -0700

    Init intro component

---
+++ frontend/src/intro/index.tsx
@@ -0,0 +1,9 @@
+import { JSX } from 'react';
+
+export default function Intro(): JSX.Element {
+  return (
+    <div className="max-w-2xl lg:max-w-6xl flex flex-col p-6 lg:px-12">
+      Intro component
+    </div>
+  )
+}
