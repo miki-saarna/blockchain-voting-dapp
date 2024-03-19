ac78ede0296215032e9667b3cf10d5a40118a2d8
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 14:57:43 -0700

    Updated intro component details

--- frontend/src/intro/index.tsx
+++ frontend/src/intro/index.tsx
@@ -3,7 +3,9 @@ import { JSX } from 'react';
 export default function Intro(): JSX.Element {
   return (
     <div className="max-w-2xl lg:max-w-6xl flex flex-col p-6 lg:px-12">
-      Intro component
+      <div className="text-xs">
+        Intro component: this component gives detailed info regarding this app.
+      </div>
     </div>
   )
 }
