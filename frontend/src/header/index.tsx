e065eda54e5de8b4deab0bb360b43e405b7d6850
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 12:25:13 -0700

    Created header component

--- frontend/src/header/index.tsx
+++ frontend/src/header/index.tsx
@@ -2,6 +2,8 @@ import { JSX } from 'react';
 
 export default function Header(): JSX.Element {
   return (
-    <header>header</header>
+    <header className="max-w-2xl lg:max-w-6xl mx-auto flex justify-between items-center p-6 lg:px-12">
+      <h1 className="text-2xl lg:text-4xl font-bold">Blockchain Voting dApp</h1>
+    </header>
   )
 }
