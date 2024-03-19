bfc089f2112e0d7836c687e2ef5300460caba31c
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 14:59:07 -0700

    Added candidates func to poll component

--- frontend/src/voting/poll.tsx
+++ frontend/src/voting/poll.tsx
@@ -1,9 +1,10 @@
-import { JSX, useState } from 'react';
+import { JSX, useState, useEffect } from 'react';
 import { getProvider, getSigner, getContract} from '../utils/blockchainInteractions';
 
 export default function Poll(): JSX.Element {
   const [pollStartTime, setPollStartTime] = useState<number>(0);
   const [pollEndTime, setPollEndTime] = useState<number>(0);
+  const [candidates, setCandidates] = useState<any[]>([]);
 
   async function beginPoll() {
     const signer = getSigner()
@@ -26,6 +27,28 @@ export default function Poll(): JSX.Element {
     await contract.endPoll();
   };
 
+  const fetchCandidates = async () => {
+    const provider = getProvider();
+    const contract = getContract(provider);
+    const candidatesLength = await contract.candidates.length; // Must create this func in smart contract
+    console.log(candidatesLength)
+    let candidatesList = [];
+    for (let i = 0; i < candidatesLength; i++) {
+      const candidate = await contract.candidates(i);
+      candidatesList.push(candidate);
+    }
+    setCandidates(candidatesList);
+    console.log(candidates)
+  };
+
+  // async function getCandidatesList() {
+  //   const provider = getProvider()
+  //   const contract = getContract(provider)
+  //   const candidates = await contract.candidates()
+  //   console.log("candidates", candidates)
+  //   return candidates;
+  // }
+
   async function castVote(candidateIdx: number) {
     const signer = getSigner()
     const contract = getContract(signer)
@@ -35,11 +58,19 @@ export default function Poll(): JSX.Element {
     console.log('tx mined')
   }
 
+  useEffect(() => {
+    fetchCandidates();
+  }, []);
+
   return (
     <div className="p-3 border border-red-300 rounded-md">
       Poll component
       <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={beginPoll}>Begin poll</button>
       <button className="border border-gray-400 px-2 py-1 rounded ml-2" onClick={() => castVote(1)}>Vote for 2nd candidate</button>
+
+      <ul>
+        {candidates.map((candidate) => <li>{candidate}</li>)}
+      </ul>
     </div>
   )
 }
