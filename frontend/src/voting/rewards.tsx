49648b1f6c645ff5913b95ccea35b9a537efa43e
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 14:45:32 -0700

    Init rewards component

---
+++ frontend/src/voting/rewards.tsx
@@ -0,0 +1,9 @@
+import { JSX } from 'react';
+
+export default function Rewards(): JSX.Element {
+  return (
+    <div className="p-3 border border-red-300 rounded-md">
+      Rewards component
+    </div>
+  )
+}
