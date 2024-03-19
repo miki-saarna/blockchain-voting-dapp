f190264b0272c70e54377bfdf2bb84ed00c11fbf
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 15:08:09 -0700

    Remove unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,11 @@
 // Add these lines at the top where you have other imports
-import React, { useState, useEffect } from 'react';
 import Header from './header/index';
 import Footer from './footer/index';
 import Intro from './intro/index';
 import Voting from './voting/index'
 import MetaMaskInteractions from './utils/metaMaskInteractions';
-import { getProvider, getSigner, getContract} from './utils/blockchainInteractions';
 
 function App() {
-  const [userAccount, setUserAccount] = useState(null);
-  // const [voteCount, setVoteCount] = useState({});
-  // const [winner, setWinner] = useState([]);
-
-  // Connect to Metamask
-  async function connectWallet() {
-    if (typeof window.ethereum !== 'undefined') {
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setUserAccount(accounts[0]);
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      console.log('Please install MetaMask!');
-    }
-  }
-
-  useEffect(() => {
-    connectWallet();
-    // Optional: Listen for account changes
-    window.ethereum.on('accountsChanged', () => connectWallet());
-  }, []);
-
-  
 
   return (
     <div className="App flex flex-col min-h-screen">
