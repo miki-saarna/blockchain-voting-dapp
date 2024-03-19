af45916b43d6f17841f20a60d57f4d7f74abe253
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 15:08:02 -0700

    Implemented button component

--- frontend/src/utils/metaMaskInteractions.jsx
+++ frontend/src/utils/metaMaskInteractions.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
+import Button from '../components/button';
 
 export default function MetaMaskInteractions() {
   const [userAccount, setUserAccount] = useState(null);
-  // const [voteCount, setVoteCount] = useState({});
-  // const [winner, setWinner] = useState([]);
 
-  // Connect to Metamask
   async function connectWallet() {
     if (typeof window.ethereum !== 'undefined') {
       try {
@@ -27,7 +25,13 @@ export default function MetaMaskInteractions() {
 
   return (
     <div className="max-w-2xl lg:max-w-6xl flex flex-col space-y-6 p-6 lg:px-12">
-      <button className="border border-gray-400 px-2 py-1 rounded" onClick={connectWallet}>Connect to MetaMask</button>
+      <Button
+        onClick={connectWallet}
+        color="bg-gray-800"
+        className="w-fit font-bold"
+      >
+        Connect to MetaMask
+      </Button>
       <p className="text-xs"><span className="font-bold">MetaMask account:</span> {userAccount}</p>
     </div>
   )
