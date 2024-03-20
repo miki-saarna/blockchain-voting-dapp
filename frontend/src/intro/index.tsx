0485b0ba0e3848db252d49331f956355f44c57db
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-19 21:32:50 -0700

    Updated component spacing

--- frontend/src/intro/index.tsx
+++ frontend/src/intro/index.tsx
@@ -2,7 +2,7 @@ import { JSX } from 'react';
 
 export default function Intro(): JSX.Element {
   return (
-    <div className="max-w-2xl lg:max-w-6xl flex flex-col p-6 lg:px-12">
+    <div className="max-w-2xl lg:max-w-6xl flex flex-col px-6 lg:px-12">
       <div className="text-xs">
         Intro component: this component gives detailed info regarding this app.
       </div>
--- frontend/src/utils/metaMaskInteractions.jsx
+++ frontend/src/utils/metaMaskInteractions.jsx
@@ -24,14 +24,14 @@ export default function MetaMaskInteractions() {
   }, []);
 
   return (
-    <div className="max-w-2xl lg:max-w-6xl flex flex-col space-y-6 p-6 lg:px-12">
+    <div className="max-w-2xl lg:max-w-6xl flex flex-col mt-6 px-6 lg:px-12">
       <Button
         onClick={connectWallet}
         className="w-fit bg-teal border border-sage-dark font-bold"
       >
         Connect to MetaMask
       </Button>
-      <p className="text-xs"><span className="font-bold">MetaMask account:</span> {userAccount}</p>
+      <p className="mt-2 text-xs"><span className="font-bold">MetaMask account:</span> {userAccount}</p>
     </div>
   )
 }
