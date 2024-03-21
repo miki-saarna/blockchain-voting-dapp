fc17a9d0bb32b8d12b9144993fe4555013a97196
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-20 17:49:46 -0700

    Created textInput component

---
+++ frontend/src/components/textInput.tsx
@@ -0,0 +1,17 @@
+import { forwardRef, InputHTMLAttributes } from 'react';
+
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+  return (
+    <input
+      ref={ref}
+      placeholder="Add a new candidate"
+      type="text"
+      className="py-1.5 px-2.5 border border-sage-light focus:outline-sage rounded-md"
+      {...props}
+    />
+  )
+})
+
+export default TextInput
