88b2bd26e6fbf2a414ed74e1d547fff627f81fc1
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-20 17:57:50 -0700

    Added icon to header

--- frontend/src/layout/header.tsx
+++ frontend/src/layout/header.tsx
@@ -1,9 +1,11 @@
 import { JSX } from 'react';
+import { UsersIcon } from '@heroicons/react/24/solid'
 
 export default function Header(): JSX.Element {
   return (
-    <div className="max-w-2xl lg:max-w-6xl mx-auto w-full p-6 lg:px-12">
+    <div className="max-w-2xl lg:max-w-6xl mx-auto w-full p-6 lg:px-12 flex items-center">
       <h1 className="text-2xl lg:text-4xl font-bold">Blockchain Voting dApp</h1>
+      <UsersIcon className="ml-2 w-7 h-7" />
     </div>
   )
 }
