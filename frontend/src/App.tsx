7cb271fc524f31329246a92b16710596aeac5dfd
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-23 09:34:35 -0700

    Enhanced MetaMask connction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { JSX, useEffect } from 'react';
+import { JSX, useState, useEffect } from 'react';
 import Header from './layout/header';
 import Footer from './layout/footer';
 import Description from './layout/description';
@@ -6,6 +6,7 @@ import Voting from './voting/index'
 import MetaMaskInteractions from './utils/metaMaskInteractions';
 
 function App(): JSX.Element {
+  const [isWalletConnected, setIsWalletConnected] = useState(false)
 
   useEffect(() => {
     document.documentElement.style.background = '#fff8f0';
@@ -15,7 +16,7 @@ function App(): JSX.Element {
     <div className="flex flex-col min-h-screen w-full text-sage-dark">
       <Header />
       <Description />
-      <MetaMaskInteractions />
+      <MetaMaskInteractions isWalletConnected={isWalletConnected} setIsWalletConnected={setIsWalletConnected} />
       <Voting />
       <Footer />
     </div>
