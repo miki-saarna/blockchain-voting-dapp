12e9fd65944154cac763293e7c04351988283c09
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-03-23 10:59:33 -0700

    Updated textInput component

--- frontend/src/components/textInput.tsx
+++ frontend/src/components/textInput.tsx
@@ -2,13 +2,13 @@ import { forwardRef, InputHTMLAttributes } from 'react';
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(({className, ...props}, ref) => {
   return (
     <input
       ref={ref}
       placeholder="Add a new candidate"
       type="text"
-      className="py-1.5 px-2.5 border border-sage-light focus:outline-sage rounded-md"
+      className={`py-1.5 px-2.5 border border-sage-light focus:outline-sage rounded-md ${className}`}
       {...props}
     />
   )
